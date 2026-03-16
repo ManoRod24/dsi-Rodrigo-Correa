76624037be82304b26907263ddb944d086357c7a
Rodrigo Correa <rodrigonasc24@gmail.com>
2026-03-16 09:23:13 -0300

    Aula sobre variáveis

---
+++ aula02/Variaveis.java
@@ -0,0 +1,16 @@
+package aula02;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        String msgPt1 = "Olá";
+        String msgPt2 = "alunos";
+        System.out.println(msgPt1 + " " + msgPt2);
+
+        int n1 = 1;
+        String n2 = "2";
+        System.out.println(n1 + Integer.parseInt(n2));
+        //Soma de uma String e um Int. ("Integer.parseInt"). Vou usar isso sempre que eu quiser transformar.
+    }
+}
+
+//Concatenação: Colocar um espaço (" ")
