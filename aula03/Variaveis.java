bac8306ffdbc078816bdec21cb219f062a84b08b
Rodrigo Correa <rodrigonasc24@gmail.com>
2026-04-06 09:22:36 -0300

    aula03

---
+++ aula03/Variaveis.java
@@ -0,0 +1,11 @@
+package aula03;
+
+public class Variaveis {
+public static void main(String[] args) {
+    //Um conjunto de variável que podem ser classificadas na mesma linha.
+    int n1, n2, n3;
+    n1 = n2 = 10;
+    n3 = 5;
+    System.out.println(n1 + n2 + n3);
+ }  
+}
