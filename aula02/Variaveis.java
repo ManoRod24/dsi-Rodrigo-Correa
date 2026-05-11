817cc0113ad1c8159f69ca928cb1a84d73675602
Rodrigo Correa <rodrigonasc24@gmail.com>
2026-05-11 09:31:22 -0300

    aula07

--- aula02/Variaveis.java
+++ aula02/Variaveis.java
@@ -1,5 +1,3 @@
-package aula02;
-
 public class Variaveis {
     public static void main(String[] args) {
         String msgPt1 = "Olá";
---
+++ aula07/Switch.java
@@ -0,0 +1,9 @@
+package aula07;
+
+public class Switch {
+  public static void main(String[] args{
+
+  }
+}
+
+//Ao invéz de utlizar vários If..Else.. Podemos utilizar a decalaração Switch
