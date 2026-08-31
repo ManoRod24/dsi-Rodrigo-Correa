b4f353f5188f5b522a1cbd879a52629a10e05e36
Rodrigo Correa <rodrigonasc24@gmail.com>
2026-08-31 08:44:11 -0300

    aula

--- aula08/demo/src/main/java/rodrigo/com/demo/GreetingController.java
+++ aula08/demo/src/main/java/rodrigo/com/demo/GreetingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // Essa classe vai receber requisições HTTP e devolver dados
+@RestController // Essa classe vai receber requisições HTTP e devolver dados.
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
